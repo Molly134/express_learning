ba70be1719bc2e8d5dbbdf8940fcb1e75ccebc0a
Molly134 <1347506708@qq.com>
2020-02-22 00:54:11 +0800

    project init commit

---
+++ src/main/java/com/expressstu/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.expressstu.dao;
+
+import com.expressstu.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
